4a55842f50c55ec7ca6634308db63d64a2e8fa35
misterboya <mady.mister@gmail.com>
2024-07-18 00:38:35 +0100

    recoile easy bits code

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { RecoilRoot, useRecoilValue } from "recoil"
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from "./atoms"
+import { useMemo } from "react";
+
+function App() {
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+
+}
+
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobAtomCount = useRecoilValue(jobsAtom);
+  const notificationsAtomCount = useRecoilValue(notificationsAtom);
+  const messagingAtomCount = useRecoilValue(messagingAtom);
+
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+  
+  return (
+    <div>
+      <button>Home</button>
+
+      <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs ({jobAtomCount})</button>
+      <button>Messaging ({messagingAtomCount})</button>
+      <button>Notifications ({notificationsAtomCount})</button>
+
+      <button>Me ({totalNotificationCount})</button>
+    </div>
+  )
+}
+
+export default App
